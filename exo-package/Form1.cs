53c6a501b1642a0715bf3781ff9e3beb6964c2f9
niferreira <nicolas.ferreira64680@live.fr>
2020-06-01 10:16:57 +0200

    Ajout des sources

---
+++ exo-package/Form1.cs
@@ -0,0 +1,33 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static QRCoder.PayloadGenerator;
+
+namespace exo_package
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string content = textBox1.Text;
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(content, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(100, "#d63447", "#f6eedf");
+            pictureBox1.Image = qrCodeImage;
+        }
+    }
+}
